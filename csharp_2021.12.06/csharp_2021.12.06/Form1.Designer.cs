8412cd6e19c2e7ea07b241813fe936b47fe2648d
Davincsikod <93971158+Davincsikod@users.noreply.github.com>
2021-12-06 09:43:20 +0100

    Add files via upload

---
+++ csharp_2021.12.06/csharp_2021.12.06/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace csharp_2021._12._06
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.uj = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dgv.Location = new System.Drawing.Point(78, 12);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(639, 287);
+            this.dgv.TabIndex = 0;
+            this.dgv.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "ID";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Keretösszeg";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Számlák száma";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Számlák összege";
+            this.Column4.Name = "Column4";
+            // 
+            // uj
+            // 
+            this.uj.Location = new System.Drawing.Point(78, 324);
+            this.uj.Name = "uj";
+            this.uj.Size = new System.Drawing.Size(298, 82);
+            this.uj.TabIndex = 1;
+            this.uj.Text = "Új pályázat";
+            this.uj.UseVisualStyleBackColor = true;
+            this.uj.Click += new System.EventHandler(this.uj_Click);
+            // 
+            // exit
+            // 
+            this.exit.Location = new System.Drawing.Point(407, 324);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(310, 82);
+            this.exit.TabIndex = 2;
+            this.exit.Text = "Kilép";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.uj);
+            this.Controls.Add(this.dgv);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Button uj;
+        private System.Windows.Forms.Button exit;
+    }
+}
+
---
+++ csharp_2021.12.06/csharp_2021.12.06/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace csharp_2021._12._06
+{
+    public partial class Form1 : Form
+    {
+        public string ConnectionString { get; set; }
+
+
+        public Form1()
+        {
+            ConnectionString =
+               @"Server   = (localdb)\MSSQLLocalDB;" +
+                "Database = palyazatok;";
+            InitializeComponent();            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgvFill();
+        }
+
+        private void dgvFill()
+        {
+            using (var c = new SqlConnection(ConnectionString))
+            {
+                dgv.Rows.Clear();
+                c.Open();
+                var r = new SqlConnection(
+                 "SELECT palyazat.id, sum(palyazat.tervezetA + palyazat.tervezetC), count(szamla.szamlaszam), sum(szamla.ertek) " +
+                 "FROM koltsegtipus, palyazat.szamla" +
+                 "WHERE palyazat.id = szamla.id" +
+                 "AND szamla.koltsegtipusid = koltsegtipus.id" +
+                 "GROUP BY palyazat.id" +
+                 "ORDER BY palyazat.id ASC", c).ExecuteReader();
+                while (r.Read())
+                {
+                    dgv.Rows.Add(r[0], r[1] + " Ft", r[2] + " db", r[3] + " Ft");
+                }
+                c.Close();
+            }
+               
+            
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void uj_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
